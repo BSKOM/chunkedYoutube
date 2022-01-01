6422399765c1e55bfdaa4fde1e4c17e0c502b353
BSKOM <b.shelestua@gmail.com>
2022-01-01 19:12:08 +0200

    feat:add folder range with protorype of basic elements

---
+++ range/index.js
@@ -0,0 +1,33 @@
+console.log('Hello world!');
+const drawFrame = () => {
+  const fragment = document.createDocumentFragment();
+  const frameItemTemp = document.querySelector('#iFrameTemp');
+  const frameClone = frameItemTemp.content.cloneNode(true);
+  frameClone.querySelector('#frameTag').setAttribute('tytle', 'RS-Frame');
+  fragment.append(frameClone);
+  document.querySelector('#placeFrame').innerHTML = '';
+  document.querySelector('#placeFrame').appendChild(fragment);
+}
+// drawRanges(cl) {
+//     const inputRange = (e) => { cl.dispatcher(e) };
+//     const drawRange = (sours, dest) => {
+//       const fragment = document.createDocumentFragment();
+//       const itemsClone = sours.content.cloneNode(true);
+//       const outFrom = itemsClone.querySelector('.range-from-info');
+//       const outTo = itemsClone.querySelector('.range-to-info');
+
+//       const htmlFromTo = `
+//     <input class="progress range-from" type="range" value="${outFrom.textContent}" min="${outFrom.textContent}" max="${outTo.textContent}" step="1">
+//     <input class="progress range-to" type="range" value="${outTo.textContent}" min="${outFrom.textContent}" max="${outTo.textContent}" step="1">
+//       `;
+//       itemsClone.querySelector('.double.ranges').innerHTML = htmlFromTo;
+//       const rangeFrom = itemsClone.querySelector('.range-from');
+//       const rangeTo = itemsClone.querySelector('.range-to');
+//       setStyle(rangeFrom, rangeTo, outFrom, outTo, [outFrom.textContent, outTo.textContent]);
+//       // rangeFrom.addEventListener('input', inputRange)
+//       // rangeTo.addEventListener('input', inputRange)
+//       fragment.append(itemsClone);
+//       dest.innerHTML = '';
+//       dest.appendChild(fragment);
+//       dest.addEventListener('input', inputRange)
+// }
